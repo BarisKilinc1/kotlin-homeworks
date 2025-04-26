28ba7dc1fa16b43c3bb832af2cc7a79ecf8bfdd2
BarisKilinc1 <148693774+BarisKilinc1@users.noreply.github.com>
2025-04-26 23:42:35 +0300

    Homework1

---
+++ Homework1/homework1-file.kt
@@ -0,0 +1,65 @@
+package com.example.kotlindersleri.variables
+fun main() {
+    var sehir = "Adana"
+    var ulke= "Türkiye"
+    var telefon = "0545623198651"
+    var postaKodu = "01000"
+    var email= "odev10@outlook.com"
+    var meslek = "Bilgisayar Mühendisi"
+    var stokMiktari = 20
+    var musteriAdi = "Hasan"
+    var bakiye = 3200
+    var dogumGunu = "21-03-2001"
+    var maas = 35000
+    var medeniDurum = "Bekar"
+    var urunYorum = "Kullanışlı ve Rahat"
+
+    var odemeTarihi = "08-05-2025"
+    var odeme = 382.5
+    var siparisAdeti = 12
+    var arabaModeli = "Hyundai"
+    var kitapAdi = "Olasılıksız"
+    var yayinlamaTarihi = "06-06-2026"
+    var indirimMiktari = "%70"
+    var odaSayisi = 4
+    var enlem = "36–42 Kuzey"
+    var boylam = "26-45 Doğu"
+    var urunAdi = "Terlik"
+    var yemekFiyati = 130
+
+
+    val marka: String = "Addidas"
+    val muzikAdi: String = "Dans Et"
+    val videoSuresi: Float = 23.45f
+    val urunPuani: Float = 3.8f
+    val resimAdi: String = "Yaşlı Gitarist"
+    val dosyaFormati: String = "PDF"
+    val renk: String = "Siyah"
+    val renkKodu: String = "#000000"
+    val telefonModeli: String = "Xiaomi"
+    val ekranBoyutu: Float = 6.67f
+
+    val agirlik: Double = 201.8
+    val ulusalGun: String = "29 Ekim Cumhuriyet Bayramı"
+    val tatilGunu: String = "1 Ocak"
+    val rezervasyonTarihi: String = "28-07-2028"
+    val sokakAdi: String="62324 sk"
+    val otobusHatti: String = ("105 BALCALI-TEKNOKENT")
+    val kalanDakika: Float = 21.30f
+    val takipKodu: String="2165852"
+    val kuponSuresi: Float= 59.59f
+    val kuponKodu: String = "3FGJHN21L"
+    val faturaAdresi: String = "Adana/Seyhan"
+
+
+
+    println("$ulke, $sehir, $telefon, $postaKodu, $email, $meslek, $stokMiktari, $musteriAdi, $bakiye, $dogumGunu, $maas, $medeniDurum, $urunYorum")
+    println("$odemeTarihi, $odeme, $siparisAdeti, $arabaModeli, $kitapAdi, $yayinlamaTarihi, $indirimMiktari, $odaSayisi, $enlem, $boylam, $urunAdi, $yemekFiyati")
+    println("$marka, $muzikAdi, $videoSuresi, $urunPuani, $resimAdi, $dosyaFormati, $renk, $renkKodu, $telefonModeli, $ekranBoyutu, $agirlik,")
+    println("$ulusalGun, $tatilGunu, $rezervasyonTarihi, $sokakAdi, $otobusHatti, $kalanDakika, $takipKodu, $kuponSuresi, $kuponKodu, $faturaAdresi")
+
+
+
+}
+
+
